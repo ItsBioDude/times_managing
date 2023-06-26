b6b1cda0241899ea88cd12ea3412270c6389a5a0
ItsBioDude <locphuc016@gmail.com>
2023-06-26 18:48:27 +0700

    init project

---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import dayjs from 'dayjs'; // ES 2015
+	const date = dayjs();
+	let currentTime = date;
+	const updateTime = () => {
+		currentTime = dayjs();
+	};
+	setInterval(updateTime, 1000);
+</script>
+
+<main class="grid grid-cols-[30vw_70vw] h-screen w-screen bg-[#56585d] text-white">
+	<ul>
+		<!-- TODO List -->
+	</ul>
+
+	<div>
+		{date.format('DD MMM YYYY')}
+		{currentTime.format('HH:mm:ss A')}
+		<!-- Clock -->
+	</div>
+</main>
---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+import UnoCSS from 'unocss/vite';
+// import extractorSvelte from '@unocss/extractor-svelte';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+	plugins: [
+		UnoCSS({
+			// extractors: [extractorSvelte()]
+			/* more options */
+		}),
+		sveltekit()
+	]
+});
